433e419ac9ec95103f016a8bae3216aed0282ce2
micaelavera <micavera2010@hotmail.com>
2024-04-25 21:19:06 -0300

    add US2 unit tests

--- src/main/java/com/mascotapp/core/discovery/MascotAppDiscovery.java
+++ src/main/java/com/mascotapp/core/discovery/MascotAppDiscovery.java
@@ -17,8 +17,7 @@ import com.mascotapp.core.logger.Logger;
 import com.mascotapp.core.service.dataprovider.PetDataProvider;
 
 public class MascotAppDiscovery {
-	// Expresión regular para verificar la validez de una ruta de directorio
-	private static final String DIRECTORY_REGEX = "^[^\\s^\\x00-\\x1f\\\\?*:\"\";<>|/,][^\\x00-\\x1f\\\\?*:\"\";<>|/,]*[^,\\s^\\x00-\\x1f\\\\?*:\"\";<>|]+$";
+
 	private static final String CLASS_EXTENSION = ".class";
 	private static final String JAR_EXTENSION = ".jar";
 	
@@ -30,16 +29,16 @@ public class MascotAppDiscovery {
      * @throws IllegalArgumentException Si la ubicación especificada no es válida.
      */
     public static Set<PetDataProvider> discover(String path) throws FileNotFoundException, IllegalArgumentException {
-        File directory = new File(path);
-
-        if (!path.matches(DIRECTORY_REGEX)) {
-            throw new IllegalArgumentException("Invalid location: " + path);
-        }
+        File directory = new File(path);    
 
         if (!directory.exists()) {
-            throw new FileNotFoundException("Location does not exist: " + path);
+            throw new FileNotFoundException("File location does not exist: " + path);
         }
 
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException("Location is not a directory: " + path);
+        }
+        
         return findClasses(directory);
     }
     
--- src/test/java/userstories/UserStory2.java
+++ src/test/java/userstories/UserStory2.java
@@ -5,41 +5,65 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.FileNotFoundException;
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.mascotapp.core.discovery.MascotAppDiscovery;
 import com.mascotapp.core.service.dataprovider.PetDataProvider;
 
 public class UserStory2 {
-
-//	  MascotAppDiscovery mascotAppDiscovery;
-//	  
-//	  @BeforeEach 
-//	  void setUp() throws Exception { 
-//		  mascotAppDiscovery = new MascotAppDiscovery(); 
-//	  }
 	 
-	@Test//(expected = FileNotFoundException.class)
+	@Test
     public void CA1_discoverNonExistentPath() throws FileNotFoundException, IllegalArgumentException {
-		String nonExistentLocation = "src/test/resources/US2/nonExistentLocation";
+		String nonExistentLocationPath = "src/test/resources/US2/nonExistentLocation";
 		
-        assertThrows(FileNotFoundException.class, () -> MascotAppDiscovery.discover(nonExistentLocation));
+        assertThrows(FileNotFoundException.class, () -> MascotAppDiscovery.discover(nonExistentLocationPath));
     }
 
-	/*@Test -- VER NO PASA EL TEST
-	 * public void CA2_discoverInvalidLocationPath() throws FileNotFoundException, IllegalArgumentException { 
-	 * String invalidLocation = "src/test/resources/US2/file.txt";
-	 * 
-	 * assertThrows(IllegalArgumentException.class, () -> mascotAppDiscovery.discover(invalidLocation)); }
-	 */
+	@Test 
+	  public void CA2_discoverInvalidLocationPath() throws FileNotFoundException, IllegalArgumentException { 
+		String invalidLocationPath = "src/test/resources/US2/file.txt";
+
+		assertThrows(IllegalArgumentException.class, () -> MascotAppDiscovery.discover(invalidLocationPath)); 
+	  }
 
 	@Test
     public void CA3_discoverEmptyFolderPath() throws FileNotFoundException, IllegalArgumentException {
-		String emptyFolder = "src/test/resources/US2/emptyFolder";
-        Set<PetDataProvider> dataProvidersDiscovery = MascotAppDiscovery.discover(emptyFolder);
+		String emptyFolderPath = "src/test/resources/US2/emptyFolder";
+        Set<PetDataProvider> dataProvidersDiscovery = MascotAppDiscovery.discover(emptyFolderPath);
+        
+        assertTrue(dataProvidersDiscovery.isEmpty());
+    }
+	
+	@Test
+    public void CA4_discoverNotSocialNetworkPath() throws FileNotFoundException, IllegalArgumentException {
+		String notSocialNetworkPath = "src/test/resources/US2/notSocialNetwork";
+        Set<PetDataProvider> dataProvidersDiscovery = MascotAppDiscovery.discover(notSocialNetworkPath);
         
         assertTrue(dataProvidersDiscovery.isEmpty());
     }
 
+	@Test
+    public void CA5_discoverSingleSocialNetworkPath() throws FileNotFoundException, IllegalArgumentException {
+		String singleSocialNetworkPath = "src/test/resources/US2/singleSocialNetwork";
+        Set<PetDataProvider> dataProvidersDiscovery = MascotAppDiscovery.discover(singleSocialNetworkPath);
+                  
+        assertEquals(1, dataProvidersDiscovery.size());
+        
+        for (PetDataProvider elemento : dataProvidersDiscovery) {
+        	assertTrue(elemento.getClass().getSimpleName().equals("Facebook"));
+        }        	
+    }
+
+	@Test
+    public void CA6_discoverMultipleSocialNetworksPath() throws FileNotFoundException, IllegalArgumentException {
+		String multipleSocialNetworksPath = "src/test/resources/US2/multipleSocialNetworks";
+        Set<PetDataProvider> dataProvidersDiscovery = MascotAppDiscovery.discover(multipleSocialNetworksPath);
+                  
+        assertEquals(2, dataProvidersDiscovery.size());
+                
+        for (PetDataProvider elemento : dataProvidersDiscovery) {
+        	assertTrue(elemento.getClass().getSimpleName().equals("Facebook") ||
+        			   elemento.getClass().getSimpleName().equals("Instagram")); 
+        }   	
+    }
 }
